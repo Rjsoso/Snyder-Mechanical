6c79931c3875974f0d4b7bcc8901658b4ee5ae7c
Rory Bylund <rorybylund@gmail.com>
2026-01-26 13:38:11 -0700

    Increase logo size in header
    Made the logo much bigger by increasing from h-10 md:h-12 to h-16 md:h-20 for better visibility.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
             <img 
               src="/logo.png" 
               alt="Snyder Mechanical" 
-              className="h-10 md:h-12 w-auto"
+              className="h-16 md:h-20 w-auto"
             />
           </Link>
 
